b0784e2960256784e717881d7d086a18b8c796d8
Ismael Grimaldo <ismaelgrive@gmail.com>
2021-06-13 02:19:55 -0500

    testing background image

--- src/containers/Description.jsx
+++ src/containers/Description.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import '../assets/styles/components/Description.scss'
 
 const Description = () => {
+  console.log(process.env.PUBLIC_URL)
   return(
-  <section className="description-section">
+  <section className="description-section" style={{
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/image.png'})`
+  }}>
     
   </section>
   );
